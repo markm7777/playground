7e40df1cbfe6e1bd0e2258db655ae0bb4e293dde
markm7777 <mark.murphy7777@gmail.com>
2020-06-07 16:23:37 -0700

    Updates

--- precache-manifest.10e157a71587b4dd8d69265f8cfe3f0f.js
+++ precache-manifest.10e157a71587b4dd8d69265f8cfe3f0f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7f59c33f8bcedc2de0ca89fc302bed74",
+    "revision": "ceb4bfc38426e8a884226b87a7d36151",
     "url": "/playground/index.html"
   },
   {
-    "revision": "24ce99a4011669080bc9",
+    "revision": "d65125697fe7eb775d8a",
     "url": "/playground/static/css/main.781e23c3.chunk.css"
   },
   {
-    "revision": "9d235de47aebb1669b2b",
-    "url": "/playground/static/js/2.17fbc117.chunk.js"
+    "revision": "b29a98aee98b60cd47d4",
+    "url": "/playground/static/js/2.1b9b62a7.chunk.js"
   },
   {
     "revision": "8527287d17d7deb14b4f9a30d40d1e38",
-    "url": "/playground/static/js/2.17fbc117.chunk.js.LICENSE.txt"
+    "url": "/playground/static/js/2.1b9b62a7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "24ce99a4011669080bc9",
-    "url": "/playground/static/js/main.b3428582.chunk.js"
+    "revision": "d65125697fe7eb775d8a",
+    "url": "/playground/static/js/main.d0b45cde.chunk.js"
   },
   {
     "revision": "c82bad4aa24fa160f3c7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/playground/precache-manifest.0364d0305ba1ee807139153def94fb92.js"
+  "/playground/precache-manifest.10e157a71587b4dd8d69265f8cfe3f0f.js"
 );
 
 self.addEventListener('message', (event) => {
